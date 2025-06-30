eed10bb16318ea31fe8b0141aa4a9fc1b5579159
ghassendallagi <ghassen.dallegi.8@gmail.com>
2025-06-30 13:43:05 +0100

    Update ClientClient.java

--- ServiceMS/src/main/java/tn/esprit/spring/servicems/feignClients/ClientClient.java
+++ ServiceMS/src/main/java/tn/esprit/spring/servicems/feignClients/ClientClient.java
@@ -4,7 +4,6 @@ import tn.esprit.spring.servicems.entity.ServiceClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import tn.starter.mysqlShared.dto.auteurDTO;
 
 @FeignClient(name = "AuteurMS")
 public interface ClientClient {
